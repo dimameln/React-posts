0d02b0a0d5191347769706b6d22bb54763f235ac
dimameln <dima_meln@bk.ru>
2022-06-23 19:03:04 +0300

    add public and private routes

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -2,19 +2,34 @@ import React from 'react'
 import { Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom';
 import Posts from '../pages/Posts';
-import { router } from '../router/routes';
+import { privateRoutes, publicRoutes } from '../router/routes';
+import Login from '../pages/Login';
 
 export default function AppRouter() {
+  const isAuth = true
   return (
+    isAuth 
+    ? 
     <Routes>
-        {router.map(route => 
-          <Route 
-            element={route.component} 
-            path={route.path} 
-            exact={route.exact}
-          />  
-        )}
-        <Route path='*' element={<Posts/>}/>
+      {privateRoutes.map(route => 
+        <Route 
+          element={route.component} 
+          path={route.path} 
+          exact={route.exact}
+        />  
+      )}
+      <Route path='*' element={<Posts/>}/>
+    </Routes>
+    :
+    <Routes>
+      {publicRoutes.map(route => 
+        <Route 
+          element={route.component} 
+          path={route.path} 
+          exact={route.exact}
+        />  
+      )}
+      <Route path='*' element={<Login/>}/>
     </Routes>
   )
 }
