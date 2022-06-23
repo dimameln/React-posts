a9698a286d2e25811ce851fa34a2d5d6fad9b7de
dimameln <dima_meln@bk.ru>
2022-06-23 15:21:46 +0300

    Optimize routes

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom';
-import About from '../pages/About';
 import Posts from '../pages/Posts';
-import PostIdPage from '../pages/PostIdPage';
+import { router } from '../router/routes';
 
 export default function AppRouter() {
   return (
     <Routes>
-        <Route path='/about' element={<About/>}/>
-        <Route exact path='/posts' element={<Posts/>}/>
-        <Route exact path='/posts/:id' element={<PostIdPage/>}/>
+        {router.map(route => 
+          <Route 
+            element={route.component} 
+            path={route.path} 
+            exact={route.exact}
+          />  
+        )}
         <Route path='*' element={<Posts/>}/>
     </Routes>
   )
